ae9dc2b31ccaec36a7db4a70d8ef46acc424c933
Patryk Sobow <patryk.sobow95@gmail.com>
2025-06-10 15:08:40 +0200

    Init project

---
+++ src/test/java/com/sobow/reader/JsonReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobow.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
